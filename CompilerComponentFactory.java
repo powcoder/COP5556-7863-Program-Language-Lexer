7758dd0a7a6824d8f0f504736fcc53f652b3a096
powcoder <powcoder@163.com>
2022-12-05 13:23:39 +0800

    COP5556 7863 Program Language Lexer

---
+++ CompilerComponentFactory.java
@@ -0,0 +1,20 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+/**  This code is provided for solely for use of students in the course COP5556 Programming Language Principles at the 
+ * University of Florida during the Fall Semester 2022 as part of the course project.  No other use is authorized. 
+ */
+
+package edu.ufl.cise.plpfa22;
+
+public class CompilerComponentFactory {
+
+	public static ILexer getLexer(String input) {
+		return new Lexer(input);
+	}
+}
---
+++ ILexer.java
@@ -0,0 +1,29 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+/**  This code is provided for solely for use of students in the course COP5556 Programming Language Principles at the 
+ * University of Florida during the Fall Semester 2022 as part of the course project.  No other use is authorized. 
+ */
+package edu.ufl.cise.plpfa22;
+
+public interface ILexer {
+
+	/**
+	 * Return an IToken and advance the internal position so that subsequent calls will return subsequent ITokens.
+	 * @return
+	 * @throws LexicalException
+	 */
+	IToken next() throws LexicalException;
+	
+	/**
+	 * Return an IToken without advancing the internal position.  A subsequent call to next or peek will return the same IToken. 
+	 * @return
+	 * @throws LexicalException
+	 */
+	IToken peek() throws LexicalException;  
+}
